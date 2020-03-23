227d88b215ee2e3117cb3aad48c8c7fe07b2011e
Kinga Milaszkiewicz <kinga.milaszkiewicz@outlook.com>
2020-03-23 17:51:27 +0100

    Add card functionality

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,21 +2,26 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-//import Creator from '../Creator/Creator.js';
-//import { settings } from '../../data/dataStore';
+import Creator from '../Creator/Creator.js';
+import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  }
 
   static propTypes = {
     title: PropTypes.node,
     children: PropTypes.node,
     cards: PropTypes.node,
     icon: PropTypes.node,
+    addCard: PropTypes.node,
   }
 
   render() {
-    const { title, icon, cards } = this.props;
+    const { title, icon, cards, addCard } = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{title}<span className={styles.icon}><Icon name={icon} /></span></h3>
@@ -25,9 +30,9 @@ class Column extends React.Component {
             <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-        {/* <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-        </div> */}
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={addCard} />
+        </div>
       </section>
     );
   }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,13 +6,13 @@ import ReactHtmlParser from 'react-html-parser';
 const Hero = props => (
   <header className={styles.component}>
     <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
-    <img src={props.imageImg} className={styles.image}></img>
+    <img src={props.image} className={styles.image}></img>
   </header>
 );
 
 Hero.propTypes = {
   titleText: PropTypes.node.isRequired,
-  imageImg: PropTypes.string,
+  image: PropTypes.string,
 };
 
 export default Hero;
